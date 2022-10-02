20b7bcc1b58dbd59285449765c468219424887d2
Mohd Ikram <101573398+ikram1005@users.noreply.github.com>
2022-10-02 18:47:35 +0530

    Add files via upload

---
+++ Automatic Auction system/src/com/masai/exceptions/adminException.java
@@ -0,0 +1,12 @@
+package com.masai.exceptions;
+
+public class adminException extends Exception {
+       public adminException() {
+		// TODO Auto-generated constructor stub
+	}
+       public adminException(String message) {
+   		super(message);
+   	} 
+       
+       
+}
---
+++ Automatic Auction system/src/com/masai/exceptions/itemException.java
@@ -0,0 +1,13 @@
+package com.masai.exceptions;
+
+public class itemException extends Exception{
+
+	public itemException() {
+		
+	}
+    public itemException(String mess) {
+		super(mess);
+	}
+	
+   
+}
